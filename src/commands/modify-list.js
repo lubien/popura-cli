5aa088cf9f5352f4cd051cae0c856d1d657dff3b
lubien <lubien1996@gmail.com>
2016-07-23 01:37:59 -0300

    Better readability

--- src/commands/modify-list.js
+++ src/commands/modify-list.js
@@ -6,7 +6,7 @@ const debug = require('debug')('popura-cli:modify-list');
 
 export default function modifyListCommandFactory(action) {
 	return async function modifyListCommand(id, {type = 'anime'}) {
-		const method = `${action}${type[0].toUpperCase()}${type.slice(1)}`;
+		const method = `${action}${type === 'anime' ? 'Anime' : 'Manga'}`;
 		debug(`Modifying list with ${method} for ID ${id}`);
 
 		const spinner = ora('Processing').start();
