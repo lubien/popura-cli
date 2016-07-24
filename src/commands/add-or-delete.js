5f4d2b4373b7876ad432b7231ff0808bed38c180
lubien <lubien1996@gmail.com>
2016-07-24 02:18:06 -0300

    Support pattern in add-or-delete

--- src/commands/add-or-delete.js
+++ src/commands/add-or-delete.js
@@ -1,13 +1,20 @@
 import ora from 'ora';
 import {green, red} from 'chalk';
 import user from '../user';
+import {findByName} from '../utils';
 
 const debug = require('debug')('popura-cli:modify-list');
 
 export default function addOrDeleteCommandFactory(action) {
-	return async function addOrDeleteCommand(id, {type = 'anime'}) {
+	return async function addOrDeleteCommand(who, {type = 'anime'}) {
 		const method = `${action}${type === 'anime' ? 'Anime' : 'Manga'}`;
 		debug(`Modifying list with ${method} for ID ${id}`);
+
+		// Why not support regex 'who' in add?
+		// Because we search in the list :p
+		const id = isNaN(who) && action === 'delete' ?
+			(await findByName(who, type)) :
+			who;
 
 		const spinner = ora('Processing').start();
 
