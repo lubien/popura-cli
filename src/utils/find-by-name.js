2ec21de13306f677c501814ba3792cb52bcb389c
lubien <lubien1996@gmail.com>
2016-07-24 00:06:02 -0300

    Better findByName

--- src/utils/find-by-name.js
+++ src/utils/find-by-name.js
@@ -1,10 +1,38 @@
 import user from '../user';
 
-export default async function findByName(who, type) {
-	const method = type === 'anime' ?
-		'searchAnimes' :
-		'searchManga';
+const debug = require('debug')('popura-cli:find-by-name');
 
-	return await user[method](who);
+export default async function findByName(who, type) {
+	debug(`Searching for ${type} matching ${who}`);
+
+	const matcher = new RegExp(who, 'i');
+
+	const method = type === 'anime' ?
+		'getAnimeList' :
+		'getMangaList';
+
+	const {list} = await user[method]();
+
+	const matched = list
+		.filter(series => matcher.test(series.series_title))
+		.sort((a, b) => {
+			if (a.series_title < b.series_title) {
+				return -1;
+			}
+
+			if (a.series_title > b.series_title) {
+				return 1;
+			}
+
+			return 0;
+		})
+		.shift();
+
+	if (!matched) {
+		return 0;
+	}
+
+	// eslint-disable-next-line
+	return type === 'anime' ? matched.series_animedb_id : series_mangadb_id;
 }
 
